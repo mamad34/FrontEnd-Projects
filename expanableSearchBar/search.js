ff0a242f00b98802bfb9230526d24b9363378815
mohammad <mohammadtt77@yahoo.com>
2021-10-27 14:00:26 +0330

    finished

---
+++ expanableSearchBar/search.js
@@ -0,0 +1,11 @@
+const searchBtn = document.getElementById("search-btn");
+const searchInput = document.querySelector(".search-input");
+
+searchBtn.addEventListener("click", () => {
+  //searchInput.classList.add("active-search");
+  searchInput.classList.toggle("active-search");
+});
+
+document.querySelector("main").addEventListener("click", () => {
+  searchInput.classList.remove("active-search");
+});
